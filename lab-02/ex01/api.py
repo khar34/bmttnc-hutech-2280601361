7d1fe3379040be4bb571cb3c9695d8c229f07dad
Phạm Hoàng Tuấn Kha <44925409+khar34@users.noreply.github.com>
2025-05-29 14:21:50 +0700

    feat: add UI for ceasar cipher

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -100,6 +100,13 @@ def transpostion_encrypt():
     encrypted_text = transposition_cipher.encrypt(plain_text, key)
     return jsonify({'encrypted_text': encrypted_text})
 
+@app.route("/api/transposition/decrypt", methods=["POST"])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 #Main
 if __name__ == "__main__":
