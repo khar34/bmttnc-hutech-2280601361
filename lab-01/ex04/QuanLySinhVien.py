d5806472c510583f3c0ba0da4fee6fed2575b0c8
khar34 <tuankha3401567@gmail.com>
2025-06-07 14:18:12 +0700

    [add] fix

--- lab-01/ex04/QuanLySinhVien.py
+++ lab-01/ex04/QuanLySinhVien.py
@@ -1,5 +1,3 @@
-from re import search
-
 from SinhVien import SinhVien
 
 class QuanLySinhVien:
@@ -7,88 +5,123 @@ class QuanLySinhVien:
 
     def generateID(self):
         maxId = 1
-        if (self.soLuongSinhVien() > 0):
+        if self.soLuongSinhVien() > 0:
             maxId = self.listSinhVien[0]._id
             for sv in self.listSinhVien:
-                if (maxId < sv._id):
+                if maxId < sv._id:
                     maxId = sv._id
             maxId = maxId + 1
         return maxId
 
     def soLuongSinhVien(self):
-        return self.listSinhVien.__len__()
+        return len(self.listSinhVien)
 
     def nhapSinhVien(self):
         svId = self.generateID()
         name = input("Nhap ten sinh vien: ")
+        if not name.strip():
+            print("Ten khong duoc de trong!")
+            return
         sex = input("Nhap gioi tinh sinh vien: ")
+        if not sex.strip():
+            print("Gioi tinh khong duoc de trong!")
+            return
         major = input("Nhap chuyen nganh sinh vien: ")
-        diemTB = float(input("Nhap diem cua sinh vien: "))
+        if not major.strip():
+            print("Chuyen nganh khong duoc de trong!")
+            return
+        try:
+            diemTB = float(input("Nhap diem cua sinh vien: "))
+            if not (0 <= diemTB <= 10):
+                print("Diem TB phai nam trong khoang 0 den 10!")
+                return
+        except ValueError:
+            print("Vui long nhap mot so thuc hop le!")
+            return
         sv = SinhVien(svId, name, sex, major, diemTB)
         self.xepLoaiHocLuc(sv)
         self.listSinhVien.append(sv)
 
     def updateSinhVien(self, ID):
-        sv:SinhVien = self.findByID[ID]
-        if (sv != None):
+        sv: SinhVien = self.findByID(ID)
+        if sv is not None:
             name = input("Nhap ten sinh vien: ")
+            if not name.strip():
+                print("Ten khong duoc de trong!")
+                return
             sex = input("Nhap gioi tinh sinh vien: ")
+            if not sex.strip():
+                print("Gioi tinh khong duoc de trong!")
+                return
             major = input("Nhap chuyen nganh sinh vien: ")
-            diemTB = float(input("Nhap diem cua sinh vien: "))
+            if not major.strip():
+                print("Chuyen nganh khong duoc de trong!")
+                return
+            try:
+                diemTB = float(input("Nhap diem cua sinh vien: "))
+                if not (0 <= diemTB <= 10):
+                    print("Diem TB phai nam trong khoang 0 den 10!")
+                    return
+            except ValueError:
+                print("Vui long nhap mot so thuc hop le!")
+                return
             sv._name = name
             sv._sex = sex
             sv._major = major
             sv._diemTB = diemTB
             self.xepLoaiHocLuc(sv)
         else:
-            print("Sinh vien co ID = {} khong ton tai.".format(ID))
+            print(f"Sinh vien co ID = {ID} khong ton tai.")
+            return
+
     def sortByID(self):
         self.listSinhVien.sort(key=lambda x: x._id, reverse=False)
 
     def sortByName(self):
-        self.listSinhVien.sort(key=lambda x: x._name, reverse=False)
+        self.listSinhVien.sort(key=lambda x: x._name.lower(), reverse=False)
 
     def sortByDiemTb(self):
-        self.listSinhVien.sort(key=lambda x: x._diemTB, reverse=False)
+        self.listSinhVien.sort(key=lambda x: x._diemTB, reverse=True)
+
+    def sortByMajor(self):
+        self.listSinhVien.sort(key=lambda x: x._major.lower(), reverse=False)
 
     def findByID(self, ID):
-        searchResult = None
-        if (self.soLuongSinhVien() > 0):
-            for sv in self.listSinhVien:
-                if (sv._id == ID):
-                    searchResult = sv
-        return searchResult
+        for sv in self.listSinhVien:
+            if sv._id == ID:
+                return sv
+        return None
 
     def findByName(self, keyword):
         listSV = []
-        if (self.soLuongSinhVien() > 0):
+        if self.soLuongSinhVien() > 0:
             for sv in self.listSinhVien:
-                if (keyword.upper() in sv._name.upper()):
+                if keyword.upper() in sv._name.upper():
                     listSV.append(sv)
         return listSV
 
     def deleteById(self, ID):
-        isDeleted = False
         sv = self.findByID(ID)
-        if (sv != None):
+        if sv is not None:
             self.listSinhVien.remove(sv)
-            isDeleted = True
-        return isDeleted
+            return True
+        return False
 
     def xepLoaiHocLuc(self, sv: SinhVien):
-        if (sv._diemTB >= 8):
+        if sv._diemTB >= 8:
             sv._hocLuc = "Gioi"
-        elif (sv._diemTB >= 6.5):
+        elif sv._diemTB >= 6.5:
             sv._hocLuc = "Kha"
-        elif (sv._diemTB >= 5):
+        elif sv._diemTB >= 5:
             sv._hocLuc = "Trung binh"
         else:
             sv._hocLuc = "Yeu"
+
     def showSinhVien(self, listSV):
-        print("{:<8} {:<18} {:<8} {:<8} {<:8} {:<8}".format("ID", "Name", "Sex", "Major", "DiemTB","Hoc Luc"))
-        if (listSV.__len__() > 0):
+        print("{:<8} {:<18} {:<8} {:<12} {:<8} {:<8}".format("ID", "Name", "Sex", "Major", "DiemTB", "Hoc Luc"))
+        if len(listSV) > 0:
             for sv in listSV:
-                print("{:<8} {:<18} {:<8} {:<8} {<:8} {:<8}".format(sv._id, sv._name, sv._sex, sv._major, sv._diemTB, sv._hocLuc))
+                print("{:<8} {:<18} {:<8} {:<12} {:<8.2f} {:<8}".format(sv._id, sv._name, sv._sex, sv._major, sv._diemTB, sv._hocLuc))
         print("\n")
 
     def getListSinhVien(self):
--- lab-02/ex01/cipher/transposition/transposition.py
+++ lab-02/ex01/cipher/transposition/transposition.py
@@ -1,23 +1,36 @@
+import math
+
 class TranspositionCipher:
     def __init__(self):
         pass
 
     def encrypt(self, text, key):
         encrypted_text = ''
+        rows = math.ceil(len(text) / key)
+        grid = [''] * rows
+        for i in range(len(text)):
+            row = i // key
+            grid[row] += text[i]
         for col in range(key):
-            pointer = col
-            while pointer < len(text):
-                encrypted_text += text[pointer]
-                pointer += key
+            for row in range(rows):
+                if col < len(grid[row]):
+                    encrypted_text += grid[row][col]
         return encrypted_text
 
     def decrypt(self, text, key):
-        decrypted_text = [''] * key
-        row, col = 0, 0
-        for symbol in text:
-            decrypted_text[col] += symbol
-            col += 1
-            if col == key or (col == key - 1 and row >= len(text) % key):
-                col = 0
-                row += 1
-        return ''.join(decrypted_text)
+        rows = math.ceil(len(text) / key)
+        num_full_cols = len(text) % key if len(text) % key != 0 else key
+        grid = [''] * key
+        pos = 0
+        for col in range(key):
+            num_rows = rows if col < num_full_cols else rows - 1
+            for _ in range(num_rows):
+                if pos < len(text):
+                    grid[col] += text[pos]
+                    pos += 1
+        decrypted_text = ''
+        for row in range(rows):
+            for col in range(key):
+                if row < len(grid[col]):
+                    decrypted_text += grid[col][row]
+        return decrypted_text
